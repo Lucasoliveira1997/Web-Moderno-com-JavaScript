a5ae76643bc5fda214f04d96e0fc03dcf75b0949
Lucas Silva de Oliveira <ti.lucas@gruponortene.homeip.net>
2020-01-24 17:44:32 -0300

    fetch e axios

--- ajax/server.js
+++ ajax/server.js
@@ -15,8 +15,15 @@ app.get('/teste', (req, resp) => resp.send({dataAtual: new Date}))
 app.post('/formulario', (req, resp) => {
     resp.send({
         ...req.body,
-        id: 1
+        id: 10
     })
+})
+
+app.get('/parImpar', (req, resp) => {
+    //req.body
+    //req.query
+    //req.params
+    const par = parseInt(req.query.numero) % 2 == 0
 })
 
 app.listen(port, () => console.log(`Server is running on port ${port}`))
