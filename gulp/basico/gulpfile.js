eaa539ae6d95ce91f2d8ade12dd90629c8292073
Lucas Silva de Oliveira <ti.lucas@gruponortene.homeip.net>
2020-01-30 17:52:55 -0300

    gulp babel

--- gulp/basico/gulpfile.js
+++ gulp/basico/gulpfile.js
@@ -7,8 +7,7 @@ gulp.task('default', () => {
 gulp.task('copiar', ['antes1', 'antes2'], () => {
     gulp.src(['pastaA/**/*.txt'])
     // gulp.src('pastaA/*.txt')
-        // .pipe(transformacao1)
-        // .pipe(transformacao2)
+        // .pipe(console.log('Copiando...'))
         .pipe(gulp.dest('pastaB'))
 })
 
@@ -20,6 +19,12 @@ gulp.task('antes2', () => {
     console.log('Antes 2')
 })
 
-gulp.task('fim', () => {
-    console.log('Fim')    
+gulp.task('fim', ['fim1', 'fim2'])
+
+gulp.task('fim1', () => {
+    console.log('Fim 1')    
+})
+
+gulp.task('fim2', () => {
+    console.log('Fim 2')    
 })
