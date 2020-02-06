725bcfe320ef73d91fcdba2c2690d1f9fc8bbf69
Lucas Silva de Oliveira <ti.lucas@gruponortene.homeip.net>
2020-02-06 17:59:48 -0300

    gulp spa 2

--- gulp/spa/gulpTasks/deps.js
+++ gulp/spa/gulpTasks/deps.js
@@ -1,9 +1,13 @@
 const gulp = require('gulp')
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
 
 gulp.task('deps', ['deps.css', 'deps.fonts'])
 
 gulp.task('deps.css', () => {
+    gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
 
+    
 })
 
 gulp.task('deps.fonts', () => {
--- gulp/spa/gulpfile.js
+++ gulp/spa/gulpfile.js
@@ -1,6 +1,6 @@
 const gulp = require('gulp')
 const util = require('gulp-util')
-const sequence = require('run-sequence')
+//const sequence = require('run-sequence')
 
 require('./gulpTasks/app.js')
 require('./gulpTasks/deps.js')
@@ -8,6 +8,10 @@ require('./gulpTasks/servidor.js')
 
 gulp.task('default', () => {
     if(util.env.production) {
-        sequence('deps', 'app')
+        //sequence('deps', 'app')
+        gulp.start('deps', 'app')
+    } else {
+        //sequence('deps', 'app', 'servidor')
+        gulp.start('deps', 'app', 'servidor')
     }
 })
