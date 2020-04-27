db175577c601af1a9fb9ecc4fd1e0840d6242c21
Lucas Silva de Oliveira <ti.lucas@gruponortene.homeip.net>
2020-04-27 13:27:16 -0300

    banco relacional mysql

---
+++ banco-relacional/excluirEstados.sql
@@ -0,0 +1,5 @@
+--NUNCA FAZER DELETE SEM WHERE - NUNCA!!!
+
+delete from estados where sigla = 'MN'
+
+delete from estados where id >= 1000
---
+++ banco-relacional/inserieEstados1.sql
@@ -0,0 +1,9 @@
+INSERT INTO estados (nome, sigla, regiao, populacao)
+VALUES ('Acre', 'AC', 'Norte', 0.83);
+
+INSERT INTO estados 
+    (nome, sigla, regiao, populacao)
+VALUES 
+    ('Rio de Janeiro', 'RJ', 'Sudeste', 9.4),
+    ('Amazonas', 'AM', 'Norte', 4.06),
+    ('Amapá', 'AP', 'Norte', 0.8);
